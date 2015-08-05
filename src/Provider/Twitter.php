c6663ccde007278b19884b5ab2021eefbd990ced
Greg Bowler <greg.bowler@g105b.com>
2015-08-05 12:09:48 +0100

    Import and edit Provider from oauth2-github

---
+++ src/Provider/Twitter.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace League\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Token\AccessToken;
+use Psr\Http\Message\ResponseInterface;
+
+class Twitter extends AbstractProvider
+{
+    /**
+     * Domain
+     *
+     * @var string
+     */
+    public $domain = 'https://twitter.com';
+
+    /**
+     * Api domain
+     *
+     * @var string
+     */
+    public $apiDomain = 'https://api.twitter.com';
+
+    /**
+     * Get authorization headers used by this provider.
+     *
+     * Typically this is "Bearer" or "MAC". For more information see:
+     * http://tools.ietf.org/html/rfc6749#section-7.1
+     *
+     * No default is provided, providers must overload this method to activate
+     * authorization headers.
+     *
+     * @return array
+     */
+    protected function getAuthorizationHeaders($token = null)
+    {
+        return ['Authorization' => 'Token ' . $token];
+    }
+
+    /**
+     * Get authorization url to begin OAuth flow
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return $this->apiDomain.'/oauth/authenticate';
+    }
+
+    /**
+     * Get access token url to retrieve token
+     *
+     * @param  array $params
+     *
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return $this->apiDomain.'/oauth/access_token';
+    }
+
+    /**
+     * Get provider url to fetch user details
+     *
+     * @param  AccessToken $token
+     *
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        throw new \Exception("Not Yet Implemented");
+        return "";
+    }
+
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * This should not be a complete list of all scopes, but the minimum
+     * required for the provider user interface!
+     *
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    /**
+     * Check a provider response for errors.
+     *
+     * @throws IdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  string $data Parsed response data
+     * @return void
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+
+    }
+
+    /**
+     * Generate a user object from a successful user details request.
+     *
+     * @param array $response
+     * @param AccessToken $token
+     * @return League\OAuth2\Client\Provider\ResourceOwnerInterface
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        $user = new TwitterResourceOwner($response, $response['id']);
+
+        return $user->setDomain($this->domain);
+    }
+}
---
+++ src/Provider/TwitterResourceOwner.php
@@ -0,0 +1,79 @@
+<?php namespace League\OAuth2\Client\Provider;
+
+/**
+ * @property array $response
+ * @property string $resourceOwnerId
+ */
+class TwitterResourceOwner extends GenericResourceOwner
+{
+    /**
+     * Domain
+     *
+     * @var string
+     */
+    protected $domain;
+
+    /**
+     * Get resource owner email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->response['email'] ?: null;
+    }
+
+    /**
+     * Get resource owner name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->response['name'] ?: null;
+    }
+
+    /**
+     * Get resource owner nickname
+     *
+     * @return string
+     */
+    public function getNickname()
+    {
+        return $this->response['login'] ?: null;
+    }
+
+    /**
+     * Get resource owner id
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->resourceOwnerId;
+    }
+
+    /**
+     * Get resource owner url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return trim($this->domain.'/'.$this->getNickname()) ?: null;
+    }
+
+    /**
+     * Set resource owner domain
+     *
+     * @param  string $domain
+     *
+     * @return ResourceOwner
+     */
+    public function setDomain($domain)
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+}
